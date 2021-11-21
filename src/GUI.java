52563c79f33c7a67848dbf9a694eecaaf587940e
maajid <maajid11102000@gmail.com>
2021-11-21 20:03:55 +0100

    created menubar at top

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
 
     JFrame window;
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
+
 
     public static void main(String[] args) {
 
@@ -16,6 +22,8 @@ public class GUI {
 
         createWindow();
         createTextArea();
+        createMenuBar();
+        createFileMenu();
 
         window.setVisible(true);
     }
@@ -35,5 +43,46 @@ public class GUI {
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         window.add(scrollPane);
+    }
+
+    public void createMenuBar(){
+
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+
+    }
+
+    public void createFileMenu() {
+        iNew = new JMenuItem("New");
+        menuFile.add(iNew);
+
+        iOpen = new JMenuItem("Open");
+        menuFile.add(iOpen);
+
+        iSave = new JMenuItem("Save");
+        menuFile.add(iSave);
+
+        iSaveAs = new JMenuItem("Save as");
+        menuFile.add(iSaveAs);
+
+        iExit = new JMenuItem("Exit");
+        menuFile.add(iExit);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
